5f985deec19c49a65fab90d91fc906d03f354d02
bipolat <bilalikbalpolat@gmail.com>
2021-12-24 17:44:39 +0300

    components_fix

--- src/component/App.js
+++ src/component/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import Login  from './Login';
+import User from './User';
+import NewQ  from './NewQ';
+import Leader  from './Leader';
+import NoContent  from './NoContent';
+import Navigate  from './Navigate';
+import Main  from './Main';
 
 class App extends Component
 {
     render(){
         return(
-            <div>
+            <div style={{backgroundColor: "#F0F0F5"}}>
                 <h4>
                     App
                 </h4>
+                <Login/>
+                <Navigate/>
+                <Main />
+                <User/>
+                <NewQ/>
+                <Leader/>
+                <NoContent/>
             </div>
         )
             
